76cf6ef64082873587a4d596cff9cf19133ec660
Mark Suson <mark.suson@gmail.com>
2024-03-12 15:45:34 -0400

    commit for all code. aha stack bootcamp

---
+++ src/data/pocketbase.ts
@@ -0,0 +1,102 @@
+import PocketBase from "pocketbase";
+
+import type {
+  TypedPocketBase,
+  ProjectsResponse,
+  ProjectsRecord,
+  TasksRecord,
+} from "@src/data/pocketbase-types";
+
+export const pb = new PocketBase(
+  import.meta.env.POCKETBASE_URL || process.env.POCKETBASE_URL
+) as TypedPocketBase;
+
+// globally disable auto cancellation
+pb.autoCancellation(false);
+
+export async function getProjects() {
+  const projects = await pb.collection("projects").getFullList();
+
+  return projects.sort((a, b) => getStatus(a) - getStatus(b));
+}
+
+export async function addProject(name: string) {
+  const newProject = await pb.collection("projects").create({
+    name,
+    status: "not started",
+  });
+
+  return newProject;
+}
+
+export async function getProject(id: string) {
+  let project;
+  try {
+    project = await pb.collection("projects").getOne(id);
+  } catch (error) {
+    console.log("PROJECT ERROR");
+    console.log(error);
+  }
+
+  return project;
+}
+
+export async function addTask(project_id: string, text: string) {
+  const newTask = await pb.collection("tasks").create({
+    project: project_id,
+    text,
+  });
+
+  return newTask;
+}
+
+export async function getTasks(project_id: string) {
+  const options = {
+    filter: `project = "${project_id}"`,
+  };
+  let tasks;
+  try {
+    tasks = await pb.collection("tasks").getFullList(options);
+  } catch (error) {
+    console.log(error);
+  }
+
+  return tasks;
+}
+
+function getStatus(project) {
+  switch (project.status) {
+    case "not started":
+      return 7;
+    case "on hold":
+      return 6;
+    case "started":
+      return 5;
+    case "in progress":
+      return 4;
+    case "almost finished":
+      return 3;
+    case "ongoing":
+      return 2;
+    case "done":
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+export async function deleteProject(id: string) {
+  await pb.collection("projects").delete(id);
+}
+
+export async function updateProject(id: string, data: ProjectsRecord) {
+  await pb.collection("projects").update(id, data);
+}
+
+export async function deleteTask(id: string) {
+  await pb.collection("tasks").delete(id);
+}
+
+export async function updateTask(id: string, data: TasksRecord) {
+  await pb.collection("tasks").update(id, data);
+}
